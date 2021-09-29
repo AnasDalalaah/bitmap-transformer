7052dc17aa0f553abaa84d4f6d82915e55b7c47e
Anas Dalalah <83537198+AnasDalalaah@users.noreply.github.com>
2021-09-29 22:45:28 +0300

    Lab04
    Lab04

---
+++ src/main/java/bitmap/transformer/App.java
@@ -0,0 +1,10 @@
+package bitmap.transformer;
+
+import java.io.IOException;
+
+public class App {
+  public static void main(String[] args) throws IOException {
+      Bitmap coffee = new Bitmap(args[0], args[1], args[2]);
+
+  }
+}
